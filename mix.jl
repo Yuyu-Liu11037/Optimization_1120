6d1dd09a758810518e01c3713d235ac3d123c923
Liu Yuyu <angelsofdeath0x0@gmail.com>
2022-11-21 12:45:42 +0800

    fix bug in pure.jl and mix.jl

--- mix.jl
+++ mix.jl
@@ -10,7 +10,8 @@ function solveMixed01WithoutCuts(m::Int, epsilon::Float64)
         sigma[i] = rand(Uniform(0,mu[i]))
     end
 
-    model = Model(optimizer_with_attributes(CPLEX.Optimizer, "CPXPARAM_MIP_Strategy_HeuristicEffort" => 0 , "CPXPARAM_Preprocessing_Presolve" => 0 , "CPX_PARAM_MIPSEARCH" => 1 , "CPX_PARAM_MIRCUTS" => -1 , "CPX_PARAM_FRACCUTS" => -1 , "CPX_PARAM_FLOWCOVERS" => -1))
+    model = Model(optimizer_with_attributes(CPLEX.Optimizer, "CPXPARAM_MIP_Strategy_HeuristicEffort" => 0))
+    #"CPXPARAM_Preprocessing_Presolve" => 0 , "CPX_PARAM_MIPSEARCH" => 1 , "CPX_PARAM_MIRCUTS" => -1 , "CPX_PARAM_FRACCUTS" => -1 , "CPX_PARAM_FLOWCOVERS" => -1
 
     @variable(model, x[1:n] , Bin)
     @variable(model, y[1:m] >= 0)
@@ -19,7 +20,7 @@ function solveMixed01WithoutCuts(m::Int, epsilon::Float64)
     @objective(model, Max, sum(mu[i] * x[i] for i in (1:n)') + sum(mu[i] * y[i] for i in (1:m)') - c)
 
     @constraint(model, dot(a , [x; y]) <= b)
-    @constraint(model, c^2 == ((1-epsilon)/epsilon) * ((sum(sigma[i]^2 * x[i] for i in (1:n)')) + (sum(sigma[i+n]^2 * y[i] for i in (1:m)'))))
+    @constraint(model, c^2 >= ((1-epsilon)/epsilon) * ((sum(sigma[i]^2 * x[i]^2 for i in (1:n)')) + (sum(sigma[i+n]^2 * y[i]^2 for i in (1:m)'))))
     @constraint(model, y .<= 1)
 
     JuMP.optimize!(model)
--- pure.jl
+++ pure.jl
@@ -9,17 +9,16 @@ function solvePure01WithoutCuts(n::Int, epsilon::Float64)
         sigma[i] = rand(Uniform(0,mu[i]))
     end
 
-    model = Model(optimizer_with_attributes(CPLEX.Optimizer, "CPXPARAM_MIP_Strategy_HeuristicEffort" => 0 , "CPXPARAM_Preprocessing_Presolve" => 0 , "CPX_PARAM_MIPSEARCH" => 1 , "CPX_PARAM_MIRCUTS" => -1 , "CPX_PARAM_FRACCUTS" => -1 , "CPX_PARAM_FLOWCOVERS" => -1))
-    
+    model = Model(optimizer_with_attributes(CPLEX.Optimizer, "CPXPARAM_MIP_Strategy_HeuristicEffort" => 0))
+    # "CPX_PARAM_THREADS" => 1 , "CPXPARAM_Preprocessing_Presolve" => 0
     @variable(model, x[1:n], Bin)
     @variable(model, c >= 0)
 
     @objective(model, Max, sum(mu[i] * x[i] for i in (1:n)') - c)
 
     @constraint(model, dot(a,x) <= b)
-    @constraint(model, c^2 == ((1-epsilon)/epsilon) * sum(sigma[i]^2 * x[i] for i in (1:n)'))
+    @constraint(model, c^2 >= ((1-epsilon)/epsilon) * sum(sigma[i]^2 * x[i]^2 for i in (1:n)'))
 
     JuMP.optimize!(model)
-    print(objective_value)
 end
     
